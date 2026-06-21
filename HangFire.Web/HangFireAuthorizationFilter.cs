ac0c646656befed550154f75eba2585609cc4e7f
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-06-21 23:17:59 +0530

    added authorization for login page

---
+++ HangFire.Web/HangFireAuthorizationFilter.cs
@@ -0,0 +1,13 @@
+﻿using Hangfire.Dashboard;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HangFire.Web
+{
+    public class HangFireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
