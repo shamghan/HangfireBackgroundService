8b3c119faf50836b7ca9f836cc0929cb2d8c8503
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-06-21 18:44:13 +0530

    schedule job after particular time

--- HangFire.Web/Program.cs
+++ HangFire.Web/Program.cs
@@ -1,10 +1,14 @@
 using Hangfire;
+using HangFire.Web.Jobs;
 using Microsoft.Data.SqlClient;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("HangfireConnection")!;
 
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<WebPuller>();
 builder.Services.AddHangfire(config => config
     .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
     .UseSimpleAssemblyNameTypeSerializer()
@@ -22,6 +26,53 @@ RecurringJob.AddOrUpdate("sample-job", () => Console.WriteLine("Hangfire recurri
 BackgroundJob.Enqueue(() => Console.WriteLine("Hangfire background job executed!"));
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/pull", (IBackgroundJobClient client) =>
+{
+    var url = "https://consultwithgriff.com/rss.xml";
+    var directory = $"c:\\rss";
+    var filename = "consultwithgriff.json";
+    var tempPath = Path.Combine(directory, filename);
 
+    // TODO: background work
+    client.Enqueue<WebPuller>(p => p.GetRssItemUrlsAsync(url, tempPath));
+});
+app.MapGet("/sync", (IBackgroundJobClient client) =>
+{
+    var directory = $"c:\\rss";
+    var filename = "consultwithgriff.json";
+
+    var path = Path.Combine(directory, filename);
+    var json = File.ReadAllText(path);
+    var rssItemUrls = JsonSerializer.Deserialize<List<string>>(json);
+
+    var outputPath = Path.Combine(directory, "output");
+    if (!Directory.Exists(outputPath))
+        Directory.CreateDirectory(outputPath);
+
+    if (rssItemUrls == null || rssItemUrls.Count == 0)
+        return;
+
+    var delayInSeconds = 5;
+
+    foreach (var url in rssItemUrls)
+    {
+        var u = new Uri(url);
+        var stub = u.Segments.Last();
+
+        // trim trailing slash, if any and add .html
+        if (stub.EndsWith("/"))
+            stub = stub.Substring(0, stub.Length - 1);
+
+        stub += ".html";
+
+        var filePath = Path.Combine(outputPath, stub);
+        var dt = DateTimeOffset.UtcNow.AddSeconds(delayInSeconds);
+        client.Schedule<WebPuller>(
+            p => p.DownloadFileFromUrl(url, filePath),
+            dt);
+
+        delayInSeconds += 5;
+    }
+});
 app.Run();
 
